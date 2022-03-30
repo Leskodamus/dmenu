95170fb1cadd2cf60384abc65f59da097af06f0b
Leskodamus <florian@leskovsek.dev>
2022-03-30 20:41:30 +0200

    udpated config

---
+++ README.md
@@ -0,0 +1,21 @@
+# Fork of dmenu 
+
+The [suckless dmenu (dynamic menu for X)](https://tools.suckless.org/dmenu/).
+
+## Patches applied
+
++ caseinsensitive
++ highlight
++ numbers
+
+## Configuration
+
+Simply modify the `config.h` file. 
+Default configuration is stored in `config.def.h`.
+
+## Installation
+
+```
+sudo make clean install
+```
+
--- config.def.h
+++ config.def.h
@@ -4,16 +4,16 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"IBMPlexMono:size=12"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeSelHighlight] = { "#ffc978", "#005577" },
-	[SchemeNormHighlight] = { "#ffc978", "#222222" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeNorm] = { "#ffffff", "#222222" },
+	[SchemeSel] = { "#ffffff", "#1303fc" },
+	[SchemeOut] = { "#ffffff", "#1303fc" },
+	[SchemeSelHighlight] = { "#ffffff", "#1303fc" },
+    [SchemeNormHighlight] = { "#ffffff", "#1303fc" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
